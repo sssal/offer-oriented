ad40d5adeaa2b3252a44e19e8f994ca63cb27fd6
sssal <1225941299@qq.com>
2019-07-23 14:17:14 +0800

    字符串排列(字典序全排列)

--- src/permutation/Solution.java
+++ src/permutation/Solution.java
@@ -1,29 +1,26 @@
 package permutation;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
-    ArrayList<String> strings = new ArrayList<>();
+    private ArrayList<String> strings = new ArrayList<>();
 
     public ArrayList<String> Permutation(String str) {
         char[] chars = str.toCharArray();
 //        Arrays.sort(chars);
-        recursive(chars,0);
+        recursive(chars, 0);
         Collections.sort(strings);
         return strings;
 
     }
 
-    private void recursive(char[] chars, int i){
-        if(chars.length - i == 1){
+    private void recursive(char[] chars, int i) {
+        if (chars.length - i == 1) {
             strings.add(new String(chars));
-        }else{
+        } else {
             Set<Character> charSet = new HashSet<>();
-            for(int j = i;j<chars.length;j++){
-                if(!charSet.contains(chars[j])) {
+            for (int j = i; j < chars.length; j++) {
+                if (!charSet.contains(chars[j])) {
                     charSet.add(chars[j]);
                     char[] temp = chars.clone();
                     swap(temp, i, j);
@@ -33,17 +30,62 @@ public class Solution {
         }
     }
 
-    private void swap(char[] chars, int i, int j){
+    private void swap(char[] chars, int i, int j) {
         char temp = chars[j];
         chars[j] = chars[i];
         chars[i] = temp;
     }
 
-    public static void main(String[] args){
+    public ArrayList<String> PerMutationNoRecursive(String str) {
+        if (str == null) {
+            return null;
+        }
+        if (str.equals("")) {
+            return new ArrayList<String>();
+        }
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add(new String(chars));
+        while (true) {
+            int i;
+            int llabel = -1;
+            for (i = chars.length - 1; i > 0; i--) {
+                if (chars[i] > chars[i - 1]) {
+                    llabel = i - 1;
+                    break;
+                }
+            }
+            if (llabel == -1) {
+                break;
+            }
+            int j = chars.length - 1;
+            while (chars[j] <= chars[llabel]) {
+                j--;
+            }
+            int rlabel = j;
+            char temp = chars[llabel];
+            chars[llabel] = chars[rlabel];
+            chars[rlabel] = temp;
+            for (int l = llabel + 1, r = chars.length - 1; l <= r; l++, r--) {
+                temp = chars[l];
+                chars[l] = chars[r];
+                chars[r] = temp;
+            }
+
+            strings.add(new String(chars));
+        }
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
         Solution solution = new Solution();
-        ArrayList<String> strings = solution.Permutation("cba");
-        for(String str: strings){
+        ArrayList<String> strings = solution.PerMutationNoRecursive("");
+        for (String str : strings) {
             System.out.println(str);
         }
+
+
     }
 }
